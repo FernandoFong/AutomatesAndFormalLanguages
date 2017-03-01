42be54be98e9f6ca78d2ac266100990b2c8c8bef
FerFong <ferfong@localhost.localdomain>
2017-02-28 22:50:55 -0600

    Starting the proyect LOL

---
+++ proyects/proyect1/Determinista.java
@@ -0,0 +1,9 @@
+/**
+ *Proyecto de la materia de Autómatas y Lenguajes Formales.
+ *@author Fong Baeza Luis Fernando Yang.
+ *@date 28 / 02 / 2017
+ *@version 1.0
+ */
+public class Determinista{
+
+}
